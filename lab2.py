c8a6d7a73c2505055a10b7d1389b052e8995a0b9
Fireonik <54893069+Fireonik@users.noreply.github.com>
2020-12-04 21:15:38 +0200

    final?

--- lab2.py
+++ lab2.py
@@ -48,25 +48,6 @@ def message_thread():
     messagebox.showinfo(title="NERV", message="Timer expired")
 
 
-def interval_formatted(i):
-    global timers
-    curr = timers[i].interval
-    temp = [str(math.floor(curr / 86400)), str(math.floor((curr % 86400) / 3600)),
-            str(math.floor(((math.floor(curr % 86400)) % 3600) / 60)), str(((curr % 86400) % 3600) % 60)]
-    stop = False
-    for i in range(3):
-        if temp[i] == '0' and stop is False:
-            temp[i] = ''
-        else:
-            stop = True
-            if int(temp[i]) < 10:
-                temp[i] = "0" + temp[i]
-            temp[i] += ":"
-    if int(temp[3]) < 10:
-        temp[3] = '0' + temp[3]
-    return temp[0] + temp[1] + temp[2] + temp[3]
-
-
 def delete(id_):
     for index in range(len(timers)):
         if timers[index].id == id_:
@@ -86,12 +67,9 @@ def filter_setting():
     filter_setting_window = Toplevel()
     filter_setting_window.attributes("-topmost", True)
     filter_setting_window.geometry('309x39' + '+' + str(root.winfo_rootx() + 60) + '+' + str(root.winfo_rooty() + 50))
-    Button(filter_setting_window, text="Timers", command=lambda : set_state('Timer'), font=font.Font(family='Bodoni MT', size=14),
-           foreground='#622651').grid(row=0, column=0, ipadx=10)
-    Button(filter_setting_window, text="Everything", command=lambda : set_state('Everything'),  font=font.Font(family='Bodoni MT', size=14),
-           foreground='#622651').grid(row=0, column=1, ipadx=10)
-    Button(filter_setting_window, text="Alarms", command=lambda : set_state('Alarm'),  font=font.Font(family='Bodoni MT', size=14),
-           foreground='#622651').grid(row=0, column=2, ipadx=10)
+    Button(filter_setting_window, text="Timers", command=lambda: set_state('Timer'), font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=0, ipadx=10)
+    Button(filter_setting_window, text="Everything", command=lambda: set_state('Everything'),  font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=1, ipadx=10)
+    Button(filter_setting_window, text="Alarms", command=lambda: set_state('Alarm'),  font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=2, ipadx=10)
 
 
 def refresh_timer_list():
@@ -101,7 +79,6 @@ def refresh_timer_list():
             timer_list.create_window(260, (40 * j) + 15, window=timers[j].button[0], anchor='ne')
 
     def sort_by_type():  # it is so brutal because tkinter cant temporarily remove widget from canvas
-
         # get space to put stuff into
         canvas_size = 100+40*len(timers)
         timer_list.config(scrollregion=(0, 0, 265, 2 * canvas_size))
@@ -141,44 +118,36 @@ def refresh_timer_list():
 @thread
 def count():
     def one_thread_is_enough():
-        global timers, timer_list, timer_was_deleted, bodoni26
+        global timers, timer_list, timer_was_deleted, bodoni26, volume_is_on
 
-        def timer_ticking(ind):
+        def the_time_has_come(ind):
             try:
-                timers[ind].interval -= 1
-                timers[ind].label_text.set(interval_formatted(ind))
-                if timers[ind].interval == 0:
-                    sound_thread(timers[ind].signal + ".mp3")
-                    message_thread()
-                    timers[ind].label[0].destroy()
-                    timers[ind].button[0].destroy()
-                    del timers[ind]
-                    return True
-                return False
+                if timers[ind].type == 'Timer':
+                    timers[ind].interval -= 1
+                    timers[ind].label_text.set(interval_formatted(ind))
+                    if timers[ind].interval == 0:
+                        return True
+                    return False
+                else:
+                    curr = time.localtime()
+                    alarm = timers[ind].time_point
+                    if curr.tm_mon == alarm.get('month') and curr.tm_mday == alarm.get('day') and curr.tm_hour == alarm.get('hour') and curr.tm_min == alarm.get('minute') and curr.tm_sec == alarm.get('second'):
+                        return True
+                    return False
             except IndexError:
                 pass
-
-        def alarm_checking(ind):
-            try:
-                curr = time.localtime()
-                alarm = timers[ind].time_point
-                if curr.tm_mon == alarm.get('month') and curr.tm_mday == alarm.get('day') and curr.tm_hour == alarm.get('hour') and curr.tm_min == alarm.get('minute') and curr.tm_sec == alarm.get('second'):
-                    sound_thread(timers[ind].signal + '.mp3')
-                    message_thread()
-                    timers[ind].label[0].destroy()
-                    timers[ind].button[0].destroy()
-                    del timers[ind]
-                    return True
-                return False
-            except IndexError:
-                pass
-
         size = len(timers)
         timer_list.config(scrollregion=(0, 0, 265, 100+40*size))
         if size > 0:
             for i in range(size):
                 try:
-                    if timers[i].type == 'Timer' and timer_ticking(i) or timers[i].type == 'Alarm' and alarm_checking(i):
+                    if the_time_has_come(i):
+                        if volume_is_on:
+                            sound_thread('lab2/sound_signals/' + timers[i].signal + '.mp3')
+                            message_thread()
+                        timers[i].label[0].destroy()
+                        timers[i].button[0].destroy()
+                        del timers[i]
                         timer_was_deleted = True
                 except IndexError:
                     pass
@@ -217,46 +186,23 @@ def scroll_timer_list(event):
     timer_list.yview_scroll(int(event.delta / 120), "units")
 
 
-def timer_setup():
-    def confirm_add(event=None):
-        interval = eval(days.get())*86400 + eval(hours.get())*3600 + eval(minutes.get())*60 + eval(seconds.get())
-        if interval <= 0:
-            messagebox.showwarning(title='NERV', message='Invalid interval')
-            return
-
-        timers.append(Timer())
-        timers[-1].type = 'Timer'
-        timers[-1].id = generate_id()
-        timers[-1].interval = interval
-        timers[-1].signal = signal.get()
-        timers[-1].label_text.set(interval_formatted(-1))
-        timers[-1].label.append(Label(timer_list, textvariable=timers[-1].label_text, font=bodoni26, foreground='#622651'))
-        the_id = deepcopy(timers[-1].id)
-        timers[-1].button.append(Button(timer_list, image=remove_icon, command=lambda: delete(the_id), relief='flat', highlightthickness=1, bd=0))
-        timer_list.create_window(240, 40 * (len(timers) - 1), window=timers[-1].label[0], anchor='ne')
-        timer_list.create_window(260, (40 * (len(timers) - 1)) + 15, window=timers[-1].button[0], anchor='ne')
-        timer_setup_window.destroy()
-
-    timer_setup_window = Toplevel()
-    timer_setup_window.attributes("-topmost", True)
-    timer_setup_window.geometry('435x58' + '+' + str(root.winfo_rootx() + 50) + '+' + str(root.winfo_rooty() + 50))
-    bodoni14 = font.Font(family="Bodoni MT", size=14)
-    days = Spinbox(timer_setup_window, from_=0, to=365, width=3, font=bodoni14, state='readonly', foreground='#622651')
-    days.grid(row=1, column=0, padx=1)
-    hours = Spinbox(timer_setup_window, from_=0, to=23, width=2, font=bodoni14, state='readonly', foreground='#622651')
-    hours.grid(row=1, column=1)
-    minutes = Spinbox(timer_setup_window, from_=0, to=59, width=2, font=bodoni14, state='readonly', foreground='#622651')
-    minutes.grid(row=1, column=2)
-    seconds = Spinbox(timer_setup_window, from_=0, to=59, width=2, font=bodoni14, state='readonly', foreground='#622651')
-    seconds.grid(row=1, column=3)
-    signal = Spinbox(timer_setup_window, values=('Plan', 'TimerExpired', 'AlarmWentOff', "Auratone", "Softchime", "Happyday", "Ping1", "Ping2"), font=bodoni14, state='readonly', foreground='#622651')
-    signal.grid(row=1, column=4)
-    Button(timer_setup_window, command=confirm_add, text="OK", font=font.Font(family='Bodoni MT', size=16)).grid(row=0, column=5, rowspan=2, columnspan=2, ipady=6, padx=3, ipadx=15)
-    Label(timer_setup_window, text='ddd', font=bodoni14, foreground='#622651').grid(row=0, column=0, sticky='w')
-    Label(timer_setup_window, text='hh', font=bodoni14, foreground='#622651').grid(row=0, column=1, sticky='w')
-    Label(timer_setup_window, text='mm', font=bodoni14, foreground='#622651').grid(row=0, column=2, sticky='w')
-    Label(timer_setup_window, text='ss', font=bodoni14, foreground='#622651').grid(row=0, column=3, sticky='w')
-    Label(timer_setup_window, text='signal', font=bodoni14, foreground='#622651').grid(row=0, column=4, sticky='w')
+def interval_formatted(i):
+    global timers
+    curr = timers[i].interval
+    temp = [str(math.floor(curr / 86400)), str(math.floor((curr % 86400) / 3600)),
+            str(math.floor(((math.floor(curr % 86400)) % 3600) / 60)), str(((curr % 86400) % 3600) % 60)]
+    stop = False
+    for i in range(3):
+        if temp[i] == '0' and stop is False:
+            temp[i] = ''
+        else:
+            stop = True
+            if int(temp[i]) < 10:
+                temp[i] = "0" + temp[i]
+            temp[i] += ":"
+    if int(temp[3]) < 10:
+        temp[3] = '0' + temp[3]
+    return temp[0] + temp[1] + temp[2] + temp[3]
 
 
 def time_point_formatted(mon, d, h, minute, s):
@@ -265,6 +211,22 @@ def time_point_formatted(mon, d, h, minute, s):
         if int(a[ind]) < 10:
             a[ind] = '0' + a[ind]
     return a[0] + '.' + a[1] + ' ' + a[2] + ":" + a[3] + ':' + a[4]
+
+
+def add():
+    def move_to_timer_setup(event=None):
+        timer_setup_window.destroy()
+        timer_setup()
+
+    def move_to_alarm_setup(event=None):
+        timer_setup_window.destroy()
+        alarm_setup()
+
+    timer_setup_window = Toplevel()
+    timer_setup_window.attributes("-topmost", True)
+    timer_setup_window.geometry('169x39' + '+' + str(root.winfo_rootx()+60) + '+' + str(root.winfo_rooty()+50))
+    Button(timer_setup_window, text="Timer", command=move_to_timer_setup, font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=0, ipadx=10)
+    Button(timer_setup_window, text="Alarm", command=move_to_alarm_setup, font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=1, ipadx=10)
 
 
 def alarm_setup():
@@ -314,23 +276,49 @@ def alarm_setup():
     second.grid(row=1, column=4)
     signal = Spinbox(alarm_setup_window, values=("Auratone", "Softchime", "Happyday", "Ping1", "Ping2"), font=bodoni14, state='readonly', foreground='#622651')
     signal.grid(row=1, column=5)
-    Button(alarm_setup_window, command=confirm_add, text="OK", font=font.Font(family='Bodoni MT', size=16)).grid(row=0, ipady=4,column=6, rowspan=2, columnspan=2, sticky='s', padx=5, ipadx=15)
+    Button(alarm_setup_window, command=confirm_add, text="OK", font=font.Font(family='Bodoni MT', size=16)).grid(row=0, ipady=4, column=6, rowspan=2, columnspan=2, sticky='s', padx=5, ipadx=15)
 
 
-def add():
-    def move_to_timer_setup(event=None):
+def timer_setup():
+    def confirm_add(event=None):
+        interval = eval(days.get())*86400 + eval(hours.get())*3600 + eval(minutes.get())*60 + eval(seconds.get())
+        if interval <= 0:
+            messagebox.showwarning(title='NERV', message='Invalid interval')
+            return
+
+        timers.append(Timer())
+        timers[-1].type = 'Timer'
+        timers[-1].id = generate_id()
+        timers[-1].interval = interval
+        timers[-1].signal = signal.get()
+        timers[-1].label_text.set(interval_formatted(-1))
+        timers[-1].label.append(Label(timer_list, textvariable=timers[-1].label_text, font=bodoni26, foreground='#622651'))
+        the_id = deepcopy(timers[-1].id)
+        timers[-1].button.append(Button(timer_list, image=remove_icon, command=lambda: delete(the_id), relief='flat', highlightthickness=1, bd=0))
+        timer_list.create_window(240, 40 * (len(timers) - 1), window=timers[-1].label[0], anchor='ne')
+        timer_list.create_window(260, (40 * (len(timers) - 1)) + 15, window=timers[-1].button[0], anchor='ne')
         timer_setup_window.destroy()
-        timer_setup()
-
-    def move_to_alarm_setup(event=None):
-        timer_setup_window.destroy()
-        alarm_setup()
 
     timer_setup_window = Toplevel()
     timer_setup_window.attributes("-topmost", True)
-    timer_setup_window.geometry('169x39' + '+' + str(root.winfo_rootx()+60) + '+' + str(root.winfo_rooty()+50))
-    Button(timer_setup_window, text="Timer", command=move_to_timer_setup, font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=0, ipadx=10)
-    Button(timer_setup_window, text="Alarm", command=move_to_alarm_setup, font=font.Font(family='Bodoni MT', size=14), foreground='#622651').grid(row=0, column=1, ipadx=10)
+    timer_setup_window.geometry('435x58' + '+' + str(root.winfo_rootx() + 50) + '+' + str(root.winfo_rooty() + 50))
+    bodoni14 = font.Font(family="Bodoni MT", size=14)
+    days = Spinbox(timer_setup_window, from_=0, to=365, width=3, font=bodoni14, state='readonly', foreground='#622651')
+    days.grid(row=1, column=0, padx=1)
+    hours = Spinbox(timer_setup_window, from_=0, to=23, width=2, font=bodoni14, state='readonly', foreground='#622651')
+    hours.grid(row=1, column=1)
+    minutes = Spinbox(timer_setup_window, from_=0, to=59, width=2, font=bodoni14, state='readonly', foreground='#622651')
+    minutes.grid(row=1, column=2)
+    seconds = Spinbox(timer_setup_window, from_=0, to=59, width=2, font=bodoni14, state='readonly', foreground='#622651')
+    seconds.grid(row=1, column=3)
+    signal = Spinbox(timer_setup_window, values=('Plan', 'TimerExpired', 'AlarmWentOff', "Auratone", "Softchime", "Happyday", "Ping1", "Ping2"), font=bodoni14, state='readonly', foreground='#622651')
+    signal.grid(row=1, column=4)
+    Button(timer_setup_window, command=confirm_add, text="OK", font=font.Font(family='Bodoni MT', size=16)).grid(row=0, column=5, rowspan=2, columnspan=2, ipady=6, padx=3, ipadx=15)
+    Label(timer_setup_window, text='ddd', font=bodoni14, foreground='#622651').grid(row=0, column=0, sticky='w')
+    Label(timer_setup_window, text='hh', font=bodoni14, foreground='#622651').grid(row=0, column=1, sticky='w')
+    Label(timer_setup_window, text='mm', font=bodoni14, foreground='#622651').grid(row=0, column=2, sticky='w')
+    Label(timer_setup_window, text='ss', font=bodoni14, foreground='#622651').grid(row=0, column=3, sticky='w')
+    Label(timer_setup_window, text='signal', font=bodoni14, foreground='#622651').grid(row=0, column=4, sticky='w')
 
 
 def save():
@@ -345,19 +333,18 @@ def save():
 
     data = [timers, timer_was_deleted, filter_state]
     try:
-        with open('save.bin', 'wb') as save_file:
+        with open('lab2/save.bin', 'wb') as save_file:
             pickle.dump(data, save_file)
             exit()
     except pickle.PicklingError:
         exit(1)
 
 
-
 def load():
     global timers, timer_was_deleted, filter_state
 
     try:
-        with open('save.bin', 'rb') as save_file:
+        with open('lab2/save.bin', 'rb') as save_file:
             data = pickle.load(save_file)
     except IOError:
         return
@@ -383,11 +370,8 @@ def load():
             second = str(timers[i].time_point.get('second'))
             timers[i].label_text.set(time_point_formatted(month, day, hour, minute, second))
         timers[i].label.append(Label(timer_list, textvariable=timers[i].label_text, font=bodoni26, foreground='#622651'))
-        timers[i].button.append(Button(timer_list, image=remove_icon, relief='flat', highlightthickness=1, bd=0))
-        # handling late binding
-        def set_bind(button, index=i):
-            button.bind(sequence='<Button-1>', func=lambda event: delete(index))
-        set_bind(timers[i].button[0])
+        very_sad_icon = PhotoImage(file='lab2/icons/very_sad_Icon.gif')
+        timers[i].button.append(Button(timer_list, image=very_sad_icon, relief='flat', highlightthickness=1, bd=0))
 
     # display widgets
     filter_state = 'Everything'
@@ -396,16 +380,26 @@ def load():
     refresh_timer_list()
 
 
+def change_volume_state():
+    global volume_is_on, volume_state_icon, button_volume_state, sidebar
+    volume_state_icon = PhotoImage(file='lab2/icons/volume_off.gif') if volume_is_on else PhotoImage(file='lab2/icons/volume_on.gif')
+    volume_is_on = not volume_is_on
+    button_volume_state = Button(sidebar, image=volume_state_icon, command=change_volume_state, bd=0)
+    sidebar.create_window(14, 65, window=button_volume_state)
+
+
 root = Tk()
 
 # global variables
 timers = []
 timer_was_deleted = False
 filter_state = 'Everything'
-add_icon = PhotoImage(file="add24.gif")
-remove_icon = PhotoImage(file='remove.gif')
-filter_icon = PhotoImage(file='filter.gif')
-file = ImageTk.PhotoImage(file="C:\\Users\\Clarity\\Desktop\\SimonRed1.png")
+add_icon = PhotoImage(file="lab2/icons/add24.gif")
+remove_icon = PhotoImage(file='lab2/icons/remove.gif')
+filter_icon = PhotoImage(file='lab2/icons/filter.gif')
+volume_state_icon = PhotoImage(file='lab2/icons/volume_on.gif')
+volume_is_on = True
+file = ImageTk.PhotoImage(file="lab2/SimonRed1.png")
 bodoni26 = font.Font(family="Bodoni MT", name="Bodoni26", size=26)
 
 
@@ -425,8 +419,10 @@ sidebar = Canvas(root_frame, width=25, height=200, highlightthickness=0)
 sidebar.grid(row=0, column=1)
 button_filter = Button(sidebar, image=filter_icon, command=filter_setting, bd=0, highlightthickness=0)
 button_add = Button(sidebar, image=add_icon, command=lambda: add(), bd=0, )
+button_volume_state = Button(sidebar, image=volume_state_icon, command=change_volume_state, bd=0)
 sidebar.create_window(10, 15, window=button_add)
 sidebar.create_window(10, 40, window=button_filter)
+sidebar.create_window(14, 65, window=button_volume_state)
 
 # background setup
 window2 = Toplevel()
@@ -443,10 +439,10 @@ root.bind("<Configure>", force_background)
 sidebar.bind("<Unmap>", minimize_bg)
 root.bind("<Map>", unminimize_bg)
 
-#root.iconbitmap(r'C:\Users\Clarity\Desktop\Kamina.ico')
-#window2.iconbitmap(r'C:\Users\Clarity\Desktop\Kamina.ico')
+# capturing exit from the app to save data just in time before exit
 root.protocol("WM_DELETE_WINDOW", save)
 load()
 
+# launching the main processes
 count()
 root.mainloop()
